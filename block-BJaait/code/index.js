fb55aec44262e8c817b5d29891408af8fd3ce327
Aditya Dhanraj <pandeydhanrajaditya@gmail.com>
2023-04-26 13:07:57 +0530

    Drag and drop Complete

--- block-BJaait/code/index.js
+++ block-BJaait/code/index.js
@@ -1,10 +1,13 @@
 let root = document.querySelector(".root");
 let input = document.querySelector("input");
+let draggables = document.querySelectorAll(".box");
+let dragSrcEl = null;
 
 function handleSubmit(event) {
     if(event.keyCode !== 13 || event.target.value === "")  return;
     let box = document.createElement("div");
     box.classList.add("box");
+    box.setAttribute("draggable", "true");
     let p1 = document.createElement("p");
     p1.innerText = event.target.value;
     let p2 = document.createElement("p");
@@ -15,14 +18,45 @@ function handleSubmit(event) {
     event.target.value = "";
     box.addEventListener("dragstart", handleDragStart);
     box.addEventListener("dragend", handleDragEnd);
+    box.addEventListener("dragover", handleDragOver);
+    box.addEventListener("drop", handleDrop);
 }
 
 function handleDragStart(e) {
+    console.log("a");
     this.style.opacity = '0.4';
+    dragSrcEl = this;
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
 }
   
 function handleDragEnd(e) {
     this.style.opacity = '1';
 }
-  
+
+function handleDragOver(e) {
+    if (e.preventDefault) {
+        e.preventDefault();
+    }
+    e.dataTransfer.dropEffect = 'move';
+    return false;
+}
+
+function handleDrop(e) {
+    if (e.stopPropagation) {
+        e.stopPropagation();
+    }
+    if (dragSrcEl !== this) {
+        dragSrcEl.innerHTML = this.innerHTML;
+        this.innerHTML = e.dataTransfer.getData('text/html');
+    }
+    return false;
+}
+
 input.addEventListener("keyup", handleSubmit);
+draggables.forEach(ele => {
+    ele.addEventListener("dragstart", handleDragStart);
+    ele.addEventListener("dragend", handleDragEnd);
+    ele.addEventListener("dragover", handleDragOver);
+    ele.addEventListener("drop", handleDrop);
+})
