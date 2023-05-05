b1f8738b955bd3b4b7b208dfcafc1f88606e67f8
Aditya Dhanraj <pandeydhanrajaditya@gmail.com>
2023-05-06 00:48:32 +0530

    All jest test cases written

--- block-BJaajc/code/test.js
+++ block-BJaajc/code/test.js
@@ -1,66 +1,108 @@
 const {
-    getFullName,
-    isPalindrome,
-    getCircumfrence,
-    getArea,
+  getFullName,
+  isPalindrome,
+  getCircumfrence,
+  getArea,
 } = require("./index");
 
 describe("getFullName", () => {
+  test("returns full name when given first and last name", () => {
     const firstName = "John";
     const lastName = "Doe";
-  
-    test("returns full name when given first and last name", () => {
-      const expectedFullName = "John Doe";
-      expect(getFullName(firstName, lastName)).toBe(expectedFullName);
-    });
-  
-    test("returns only first name when last name is not provided", () => {
-      const expectedFullName = "John";
-      expect(getFullName(firstName)).toBe(expectedFullName);
-    });
-  
-    test("returns only last name when first name is not provided", () => {
-      const expectedFullName = "Doe";
-      expect(getFullName(undefined, lastName)).toBe(expectedFullName);
-    });
+    const expectedFullName = "John Doe";
+    expect(getFullName(firstName, lastName)).toBe(expectedFullName);
   });
-  
-  describe("isPalindrome", () => {
-    const palindrome = "racecar";
-    const nonPalindrome = "hello";
-  
-    test.each([
-      [palindrome, true],
-      [nonPalindrome, false],
-      ["", true],
-    ])("returns correct result for %s", (input, expectedOutput) => {
-      expect(isPalindrome(input)).toBe(expectedOutput);
-    });
+
+  test("returns only first name when last name is not provided", () => {
+    const firstName = "John";
+    const expectedFullName = "John";
+    expect(getFullName(firstName)).toBe(expectedFullName);
   });
-  
-  describe("getCircumfrence", () => {
+
+  test("returns only last name when first name is not provided", () => {
+    const lastName = "Doe";
+    const expectedFullName = "Doe";
+    expect(getFullName(undefined, lastName)).toBe(expectedFullName);
+  });
+
+  test("returns undefined when neither first nor last name are provided", () => {
+    expect(getFullName()).toBeUndefined();
+  });
+
+  test("returns undefined when first name is not a string", () => {
+    expect(getFullName(123, "Doe")).toBeUndefined();
+  });
+
+  test("returns undefined when last name is not a string", () => {
+    expect(getFullName("John", {})).toBeUndefined();
+  });
+});
+
+describe("isPalindrome", () => {
+  test("returns true for palindrome strings", () => {
+    expect(isPalindrome("racecar")).toBe(true);
+    expect(isPalindrome("A man a plan a canal Panama")).toBe(true);
+  });
+
+  test("returns false for non-palindrome strings", () => {
+    expect(isPalindrome("hello")).toBe(false);
+    expect(isPalindrome("not a palindrome")).toBe(false);
+  });
+
+  test("returns false for non-string values", () => {
+    expect(isPalindrome(123)).toBe(false);
+    expect(isPalindrome(null)).toBe(false);
+  });
+
+  test("returns false for strings containing only whitespace", () => {
+    expect(isPalindrome("   ")).toBe(false);
+  });
+});
+
+describe("getCircumfrence", () => {
+  test("returns correct circumference for positive radius", () => {
     const radius1 = 1;
     const radius5 = 5;
-  
-    test.each([
-      [radius1, 2 * Math.PI],
-      [radius5, 2 * Math.PI * radius5],
-      [0, 0],
-    ])("returns correct circumference for radius %d", (radius, expectedCircumference) => {
-      expect(getCircumfrence(radius)).toBe(expectedCircumference);
-    });
+    const expectedCircumference1 = "The circumference is 6.283185307179586";
+    const expectedCircumference5 = "The circumference is 31.41592653589793";
+    expect(getCircumfrence(radius1)).toBe(expectedCircumference1);
+    expect(getCircumfrence(radius5)).toBe(expectedCircumference5);
   });
-  
-  describe("getArea", () => {
+
+  test("returns undefined for null radius", () => {
+    expect(getCircumfrence(null)).toBeUndefined();
+  });
+
+  test("returns undefined for negative radius", () => {
+    expect(getCircumfrence(-5)).toBeUndefined();
+  });
+
+  test("returns undefined for non-numeric radius", () => {
+    expect(getCircumfrence("not a number")).toBeUndefined();
+  });
+});
+
+describe("getArea", () => {
+  test("returns correct area for positive radius", () => {
     const radius1 = 1;
     const radius5 = 5;
-  
-    test.each([
-      [radius1, Math.PI],
-      [radius5, Math.PI * radius5 * radius5],
-      [0, 0],
-    ])("returns correct area for radius %d", (radius, expectedArea) => {
-      expect(getArea(radius)).toBe(expectedArea);
-    });
+    const expectedArea1 = "The area is 3.141592653589793";
+    const expectedArea5 = "The area is 78.53981633974483";
+    expect(getArea(radius1)).toBe(expectedArea1);
+    expect(getArea(radius5)).toBe(expectedArea5);
   });
-  
+
+  test("returns undefined for null radius", () => {
+    expect(getArea(null)).toBeUndefined();
+  });
+
+  test("returns undefined for negative radius", () => {
+    expect(getArea(-5)).toBeUndefined();
+  });
+
+  test("returns undefined for non-numeric radius", () => {
+    expect(getArea("foo")).toBeUndefined();
+    expect(getArea({})).toBeUndefined();
+    expect(getArea([])).toBeUndefined();
+  });
+});
